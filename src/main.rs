b00faee980874d5fdfa1838671ebf96815f8f405
filipecsoares <filipecarlossoares@gmail.com>
2023-05-10 09:08:39 -0300

    feat: initialize todo with data from the db.txt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 use std::collections::HashMap;
+use std::io::Read;
+use std::str::FromStr;
 
 struct Todo {
     map: HashMap<String, bool>,
 }
 
 impl Todo {
+    fn new() -> Result<Todo, std::io::Error> {
+        // open the db file or create if not exists
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.txt")?;
+        // read the file content into a new string
+        let mut content = String::new();
+        f.read_to_string(&mut content)?;
+        // get the string and convert into a hashmap with string and bool
+        let map: HashMap<String, bool> = content
+            .lines()
+            .map(|line| line.splitn(2, '\t').collect::<Vec<&str>>())
+            .map(|v| (v[0], v[1]))
+            .map(|(k, v)| (String::from(k), bool::from_str(v).unwrap()))
+            .collect();
+        Ok(Todo { map })
+    }
+
     fn insert(&mut self, key: String) {
         self.map.insert(key, true);
     }
@@ -22,9 +44,7 @@ impl Todo {
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action."); 
     let item = std::env::args().nth(2).expect("Please specify an item.");
-    let mut todo = Todo {
-        map: HashMap::new(),
-    };
+    let mut todo = Todo::new().expect("Initialization of db failed!");
     if action == "add" {
         todo.insert(item);
         match todo.save() {
