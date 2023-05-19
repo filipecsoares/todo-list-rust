eae66c23405ff2dc76aa778f4bf2c055bf8c2f77
filipecsoares <filipecarlossoares@gmail.com>
2023-05-19 08:49:01 -0300

    refact: extract todo to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,80 +1,10 @@
-use std::collections::HashMap;
+mod todo;
+
 use std::io;
-
-struct Todo {
-    map: HashMap<String, bool>,
-}
-
-impl Todo {
-    fn new() -> Result<Todo, std::io::Error> {
-        // open the db file or create if not exists
-        let f = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .read(true)
-            .open("db.json")?;
-        // serialize json as HashMap
-        match serde_json::from_reader(f) {
-            Ok(map) => Ok(Todo { map }),
-            Err(e) if e.is_eof() => Ok(Todo {
-                map: HashMap::new(),
-            }),
-            Err(e) => panic!("An error occurred: {}", e),
-        }
-    }
-
-    fn insert(&mut self, key: String) {
-        self.map.insert(key, true);
-    }
-
-    fn save(self) -> Result<(), Box<dyn std::error::Error>> {
-        // open db.json
-        let f = std::fs::OpenOptions::new()
-            .write(true)
-            .create(true)
-            .open("db.json")?;
-        // write to file with serde
-        serde_json::to_writer_pretty(f, &self.map)?;
-        Ok(())
-    }
-
-    fn complete(&mut self, key: &String) -> Option<()> {
-        match self.map.get_mut(key) {
-            Some(v) => Some(*v = false),
-            None => None,
-        }
-    }
-
-    fn edit(&mut self, key: &String, new_name: String) -> Option<()> {
-        if let Some(v) = self.map.remove(key) {
-            self.map.insert(new_name, v);
-            Some(())
-        } else {
-            None
-        }
-    }
-
-    fn remove(&mut self, key: &String) -> Option<()> {
-        match self.map.remove_entry(key) {
-            Some((_k, _v)) => Some(()),
-            None => None,
-        }
-    }
-
-    fn show(&self) {
-        for (k, v) in self.map.clone().into_iter() {
-            if v {
-                println!("[ ] {}", k);
-            } else {
-                println!("[X] {}", k);
-            }
-        }
-    }
-}
 
 fn main() {
     let action = std::env::args().nth(1).expect("Please specify an action.");
-    let mut todo = Todo::new().expect("Initialization of db failed!");
+    let mut todo = todo::Todo::new().expect("Initialization of db failed!");
     if action == "add" {
         let item = read_task_name();
         todo.insert(item);
---
+++ src/todo.rs
@@ -0,0 +1,72 @@
+use std::collections::HashMap;
+
+pub struct Todo {
+    map: HashMap<String, bool>,
+}
+
+impl Todo {
+    pub fn new() -> Result<Todo, std::io::Error> {
+        // open the db file or create if not exists
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .read(true)
+            .open("db.json")?;
+        // serialize json as HashMap
+        match serde_json::from_reader(f) {
+            Ok(map) => Ok(Todo { map }),
+            Err(e) if e.is_eof() => Ok(Todo {
+                map: HashMap::new(),
+            }),
+            Err(e) => panic!("An error occurred: {}", e),
+        }
+    }
+
+    pub fn insert(&mut self, key: String) {
+        self.map.insert(key, true);
+    }
+
+    pub fn save(self) -> Result<(), Box<dyn std::error::Error>> {
+        // open db.json
+        let f = std::fs::OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open("db.json")?;
+        // write to file with serde
+        serde_json::to_writer_pretty(f, &self.map)?;
+        Ok(())
+    }
+
+    pub fn complete(&mut self, key: &String) -> Option<()> {
+        match self.map.get_mut(key) {
+            Some(v) => Some(*v = false),
+            None => None,
+        }
+    }
+
+    pub fn edit(&mut self, key: &String, new_name: String) -> Option<()> {
+        if let Some(v) = self.map.remove(key) {
+            self.map.insert(new_name, v);
+            Some(())
+        } else {
+            None
+        }
+    }
+
+    pub fn remove(&mut self, key: &String) -> Option<()> {
+        match self.map.remove_entry(key) {
+            Some((_k, _v)) => Some(()),
+            None => None,
+        }
+    }
+
+    pub fn show(&self) {
+        for (k, v) in self.map.clone().into_iter() {
+            if v {
+                println!("[ ] {}", k);
+            } else {
+                println!("[X] {}", k);
+            }
+        }
+    }
+}
